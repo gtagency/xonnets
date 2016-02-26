71fd82b2646df9855c4af0382b527c82fb2be9ed
William Samuelson <rwilliamjr1@gmail.com>
2016-02-26 01:02:24 -0500

    Updated to work in Python3

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import os
 import collections
-import cPickle
+import six.moves.cPickle as cPickle
 import numpy as np
 
 class TextLoader():
@@ -14,10 +14,10 @@ class TextLoader():
         tensor_file = os.path.join(data_dir, "data.npy")
 
         if not (os.path.exists(vocab_file) and os.path.exists(tensor_file)):
-            print "reading text file"
+            print("reading text file")
             self.preprocess(input_file, vocab_file, tensor_file)
         else:
-            print "loading preprocessed files"
+            print("loading preprocessed files")
             self.load_preprocessed(vocab_file, tensor_file)
         self.create_batches()
         self.reset_batch_pointer()
@@ -30,28 +30,32 @@ class TextLoader():
         self.chars, _ = list(zip(*count_pairs))
         self.vocab_size = len(self.chars)
         self.vocab = dict(zip(self.chars, range(len(self.chars))))
-        with open(vocab_file, 'w') as f:
+        with open(vocab_file, 'wb') as f:
             cPickle.dump(self.chars, f)
-        self.tensor = np.array(map(self.vocab.get, data))
+
+        self.tensor = list(map(self.vocab.get, data))
         np.save(tensor_file, self.tensor)
 
     def load_preprocessed(self, vocab_file, tensor_file):
-        with open(vocab_file) as f:
+        with open(vocab_file, 'rb') as f:
             self.chars = cPickle.load(f)
         self.vocab_size = len(self.chars)
         self.vocab = dict(zip(self.chars, range(len(self.chars))))
         self.tensor = np.load(tensor_file)
-        self.num_batches = self.tensor.size / (self.batch_size * self.seq_length)
+        self.num_batches = int(len(self.tensor) / (self.batch_size * self.seq_length))
 
     def create_batches(self):
-        self.num_batches = self.tensor.size / (self.batch_size * self.seq_length)
-        self.tensor = self.tensor[:self.num_batches * self.batch_size * self.seq_length]
+        self.num_batches = int(len(self.tensor) / (self.batch_size * self.seq_length))
+
+        self.tensor = self.tensor[:len(self.tensor)]
         xdata = self.tensor
         ydata = np.copy(self.tensor)
         ydata[:-1] = xdata[1:]
         ydata[-1] = xdata[0]
-        self.x_batches = np.split(xdata.reshape(self.batch_size, -1), self.num_batches, 1)
-        self.y_batches = np.split(ydata.reshape(self.batch_size, -1), self.num_batches, 1)
+
+        self.x_batches = np.resize(np.resize(xdata, len(xdata) - len(xdata) % (self.batch_size * self.seq_length)), (self.num_batches, self.seq_length, self.batch_size))
+        self.y_batches = np.resize(np.resize(ydata, len(ydata) - len(ydata) % (self.batch_size * self.seq_length)), (self.num_batches, self.seq_length, self.batch_size))
+
 
 
     def next_batch(self):
@@ -61,4 +65,3 @@ class TextLoader():
 
     def reset_batch_pointer(self):
         self.pointer = 0
-
